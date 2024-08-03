db323e4966381e19a114b2d7f602df98de7af3aa
Gustavo Beretta Gonçalves <gustavoberettag@gmail.com>
2024-08-03 14:29:35 -0300

    no-scroll

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 const hamburger = document.querySelector(".hamburger");
 const nav = document.querySelector(".nav");
+const body = document.querySelector("body");
 
-hamburger.addEventListener("click", () => nav.classList.toggle("active"));
+hamburger.addEventListener("click", () => {
+  nav.classList.toggle("active");
+  body.classList.toggle("no-scroll"); // Adicione ou remova a classe "no-scroll"
+});
